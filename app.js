5f7e69271afb0f39794c47b4127894ca5f49e668
Faiz <faizkhan8225@gmail.com>
2024-09-10 23:58:19 +0530

    Using object and array types.

--- app.js
+++ app.js
@@ -2,22 +2,29 @@ var num1Element = document.getElementById("num1");
 var num2Element = document.getElementById("num2");
 var btnElement = document.querySelector("button");
 // const paraElement = document.querySelector("p") || null;
+var numResults = [];
+var textResult = [];
 function add(num1, num2) {
-    if (typeof num1 === 'number' && typeof num2 === 'number') {
+    if (typeof num1 === "number" && typeof num2 === "number") {
         return num1 + num2;
     }
-    else if (typeof num1 === 'string' && typeof num2 === 'string') {
+    else if (typeof num1 === "string" && typeof num2 === "string") {
         return num1 + " " + num2;
     }
     else {
         return +num1 + +num2;
     }
 }
+function printResult(resultObj) {
+    console.log(resultObj.val);
+}
 btnElement.addEventListener("click", function () {
     var num1 = num1Element.value;
     var num2 = num2Element.value;
     var result = add(+num1, +num2);
+    numResults.push(result);
     var stringResult = add(num1, num2);
-    console.log(result);
-    console.log(stringResult);
+    textResult.push(stringResult);
+    console.log(numResults, textResult);
+    printResult({ val: result, timestamp: new Date() });
 });
--- app.ts
+++ app.ts
@@ -3,22 +3,31 @@ const num2Element = document.getElementById("num2") as HTMLInputElement;
 const btnElement = document.querySelector("button")!;
 // const paraElement = document.querySelector("p") || null;
 
+const numResults: number[] = [];
+const textResult: string[] = [];
+
 function add(num1: number | string, num2: number | string) {
-  if (typeof num1 === 'number' && typeof num2 === 'number') {
+  if (typeof num1 === "number" && typeof num2 === "number") {
     return num1 + num2;
-  } else if (typeof num1 === 'string' && typeof num2 === 'string') {
-    return num1 + " " + num2
+  } else if (typeof num1 === "string" && typeof num2 === "string") {
+    return num1 + " " + num2;
   } else {
-    return +num1 + +num2
+    return +num1 + +num2;
   }
+}
+
+function printResult(resultObj: { val: number; timestamp: Date }) {
+  console.log(resultObj.val);
 }
 
 btnElement.addEventListener("click", () => {
   const num1 = num1Element.value;
   const num2 = num2Element.value;
   const result = add(+num1, +num2);
-  const stringResult = add(num1, num2)
-  console.log(result);
-  console.log(stringResult);
-  
+  numResults.push(result as number);
+  const stringResult = add(num1, num2);
+  textResult.push(stringResult as string);
+  console.log(numResults, textResult);
+
+  printResult({ val: result as number, timestamp: new Date() });
 });
