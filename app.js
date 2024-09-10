354f265ee5036488ad9915a74b58dbed9343e5e6
Faiz <faizkhan8225@gmail.com>
2024-09-10 23:25:48 +0530

    Working with Union types.

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 var num1Element = document.getElementById("num1");
 var num2Element = document.getElementById("num2");
 var btnElement = document.querySelector("button");
-var paraElement = document.querySelector("p") || null;
+// const paraElement = document.querySelector("p") || null;
 function add(num1, num2) {
-    return num1 + num2;
+    if (typeof num1 === 'number' && typeof num2 === 'number') {
+        return num1 + num2;
+    }
+    else if (typeof num1 === 'string' && typeof num2 === 'string') {
+        return num1 + " " + num2;
+    }
+    else {
+        return +num1 + +num2;
+    }
 }
 btnElement.addEventListener("click", function () {
     var num1 = num1Element.value;
     var num2 = num2Element.value;
     var result = add(+num1, +num2);
-    if (paraElement) {
-        paraElement.textContent = result.toString();
-    }
+    var stringResult = add(num1, num2);
+    console.log(result);
+    console.log(stringResult);
 });
