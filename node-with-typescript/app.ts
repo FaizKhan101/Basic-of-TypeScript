318285f8779c2c066f11c63f00b6897ce8aa52a7
Faiz <faizkhan8225@gmail.com>
2024-09-13 22:55:23 +0530

    Add more routes and complete CRUD functionality.

--- node-with-typescript/app.ts
+++ node-with-typescript/app.ts
@@ -1,12 +1,12 @@
-import express from "express"
-import bodyParser from "body-parser"
+import express from "express";
+import bodyParser from "body-parser";
 
-import todosRoutes from "./routes/todos"
+import todosRoutes from "./routes/todos";
 
-const app = express()
+const app = express();
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.use(todosRoutes)
+app.use(todosRoutes);
 
-app.listen(3000)
+app.listen(3000);
--- node-with-typescript/routes/todos.ts
+++ node-with-typescript/routes/todos.ts
@@ -4,7 +4,7 @@ import { Todo } from "../models/todo";
 
 const router = Router();
 
-const todos: Todo[] = [];
+let todos: Todo[] = [];
 
 router.get("/", (req, res, next) => {
   res.status(200).json({ todos: todos });
@@ -16,7 +16,29 @@ router.post("/todo", (req, res, next) => {
     text: req.body.text,
   };
 
-  todos.push(newTodo)
+  todos.push(newTodo);
+
+  res.status(200).json({ message: "Add todo successfully." });
+});
+
+router.put("/todo/:todoId", (req, res, next) => {
+  const todoId = req.params.todoId;
+
+  const todoIndex = todos.findIndex((todo) => todo.id === todoId);
+
+  if (todoIndex >= 0) {
+    todos[todoIndex] = { id: todos[todoIndex].id, text: req.body.text };
+    return res.status(200).json({ message: "Updated todo succesfully." });
+  }
+  res.status(404).json({ message: "Could not find todo with given id." });
+});
+
+router.delete("/todo/:todoId", (req, res, next) => {
+  const todoId = req.params.todoId;
+
+  const updatedTodos = todos.filter((todo) => todo.id !== todoId);
+  todos = updatedTodos;
+  res.status(200).json({ message: "Delete todo successfully." });
 });
 
 export default router;
