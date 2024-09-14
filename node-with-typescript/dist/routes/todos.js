40d433393c2b5e8ed5f49e4201bb5c32014c6a28
Faiz <faizkhan8225@gmail.com>
2024-09-14 11:22:20 +0530

    Improve folder structure.

--- node-with-typescript/dist/routes/todos.js
+++ node-with-typescript/dist/routes/todos.js
@@ -7,24 +7,28 @@ router.get("/", (req, res, next) => {
     res.status(200).json({ todos: todos });
 });
 router.post("/todo", (req, res, next) => {
+    const body = req.body;
     const newTodo = {
         id: new Date().toISOString(),
-        text: req.body.text,
+        text: body.text,
     };
     todos.push(newTodo);
     res.status(200).json({ message: "Add todo successfully." });
 });
 router.put("/todo/:todoId", (req, res, next) => {
-    const todoId = req.params.todoId;
+    const body = req.body;
+    const params = req.params;
+    const todoId = params.todoId;
     const todoIndex = todos.findIndex((todo) => todo.id === todoId);
     if (todoIndex >= 0) {
-        todos[todoIndex] = { id: todos[todoIndex].id, text: req.body.text };
+        todos[todoIndex] = { id: todos[todoIndex].id, text: body.text };
         return res.status(200).json({ message: "Updated todo succesfully." });
     }
     res.status(404).json({ message: "Could not find todo with given id." });
 });
 router.delete("/todo/:todoId", (req, res, next) => {
-    const todoId = req.params.todoId;
+    const params = req.params;
+    const todoId = params.todoId;
     const updatedTodos = todos.filter((todo) => todo.id !== todoId);
     todos = updatedTodos;
     res.status(200).json({ message: "Delete todo successfully." });
